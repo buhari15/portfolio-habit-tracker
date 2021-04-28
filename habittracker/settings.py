bdfb05079baa34b252ec5408b845c2f931dcd9e8
Buhari Abubakar <buharikwmbo@hotmail.com>
2021-04-28 17:34:58 +0200

    new

--- habittracker/settings.py
+++ habittracker/settings.py
@@ -14,8 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +104,8 @@ DATABASES = {
         'PASSWORD': 'KAduna14@',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
